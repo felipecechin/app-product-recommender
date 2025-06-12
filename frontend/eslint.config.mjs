90a4ff673bf44361c2788b1a534752d9692031bf
Felipe Cechin <ficechin@hotmail.com>
2025-06-12 18:44:33 -0300

    chore: add eslint-plugin-simple-import-sort to project

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,9 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import react from 'eslint-plugin-react';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig([
   globalIgnores(['tailwind.config.js', 'postcss.config.js']),
@@ -19,7 +20,8 @@ export default defineConfig([
     files: ['**/*.{js,jsx,mjs,cjs}'],
     plugins: {
       react,
-      prettier
+      prettier,
+      'simple-import-sort': simpleImportSort
     },
     languageOptions: {
       parserOptions: {
@@ -33,6 +35,8 @@ export default defineConfig([
       }
     },
     rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       'prettier/prettier': 'error',
       'react/jsx-uses-react': 'error',
       'react/jsx-uses-vars': 'error',
