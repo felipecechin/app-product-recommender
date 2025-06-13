21a5ad81b87cd6101ed1713321f1859e1410223d
Felipe Cechin <ficechin@hotmail.com>
2025-06-12 23:02:44 -0300

    test: add new test case to Features component

--- frontend/src/components/Form/Fields/Features/Features.test.js
+++ frontend/src/components/Form/Fields/Features/Features.test.js
@@ -36,4 +36,23 @@ describe('Componente: Features', () => {
 
     expect(onFeatureChange).toHaveBeenCalledWith(['Feature 1', 'Feature 2']);
   });
+
+  it('Deve remover uma feature existente ao desmarcar', () => {
+    const features = ['Feature 1', 'Feature 2'];
+    const selectedFeatures = ['Feature 1', 'Feature 2'];
+    const onFeatureChange = jest.fn();
+
+    render(
+      <Features
+        features={features}
+        selectedFeatures={selectedFeatures}
+        onFeatureChange={onFeatureChange}
+      />
+    );
+
+    const checkbox = screen.getByLabelText('Feature 2');
+    userEvent.click(checkbox);
+
+    expect(onFeatureChange).toHaveBeenCalledWith(['Feature 1']);
+  });
 });
